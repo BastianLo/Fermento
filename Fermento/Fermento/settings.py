be812d0e0582485ff31629ca445154cb76fc0592
BastianLo <bastian.lobe@t-online.de>
2023-04-04 19:22:40 +0200

    Base template

--- Fermento/Fermento/settings.py
+++ Fermento/Fermento/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipe_manager.apps.RecipeManagerConfig'
+    'recipe_manager',
+    'Fermento',
 ]
 
 MIDDLEWARE = [
