f78e1d7b3e13b08ca1db1f66a11597f236607f45
BastianLo <bastian.lobe@t-online.de>
2023-04-08 00:12:17 +0200

    Fix translations

--- scripts/dev_deploy.sh
+++ scripts/dev_deploy.sh
@@ -1,1 +1,5 @@
+cd Fermento
+python3 manage.py makemessages -l en -l de
+cd ..
+django-admin compilemessages > /dev/null
 docker build -t fermento:localdev . && docker compose up
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pip freeze > requirements.txt
 cd Fermento
-django-admin compilemessages
+python3 manage.py makemessages -l en -l de
+django-admin compilemessages > /dev/null
 python3 manage.py runserver
