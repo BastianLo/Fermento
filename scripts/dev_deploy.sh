9161230e40e625712b3bb653d25d84b49f78b2ec
BastianLo <bastian.lobe@t-online.de>
2023-04-08 11:51:40 +0200

    Remove makemessages & compilemessages from scripts

--- scripts/dev_deploy.sh
+++ scripts/dev_deploy.sh
@@ -1,5 +1,6 @@
-cd Fermento
-python3 manage.py makemessages -l en -l de
-cd ..
-django-admin compilemessages > /dev/null
+#cd Fermento
+#python3 manage.py makemessages -l en -l de
+#cd ..
+#django-admin compilemessages > /dev/null
+pip freeze > requirements.txt
 docker build -t fermento:localdev . && docker compose up
