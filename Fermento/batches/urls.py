ac30e8039e835400b23ebda6ec3643a41d5a70f0
BastianLo <bastian.lobe@t-online.de>
2023-04-15 17:44:16 +0200

    Initial batch models, views & templates

--- Fermento/batches/urls.py
+++ Fermento/batches/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("batch", views.index, name="index"),
-    path("qrcode", views.qrcode_overview, name="qrcode_overview"),
-    path("calender", views.calender_overview, name="calender_overview"),
+    path("batch/", views.index, name="index"),
+    path("batch/<int:batch_id>/", views.batch_by_id, name="batch_by_id"),
+    path("qrcode/", views.qrcode_overview, name="qrcode_overview"),
+    path("qrcode/<int:qrcode_id>/", views.qrcode_by_id, name="qrcode_by_id"),
+    path("qrcode/<int:qrcode_id>/redirect/", views.redirect_qrcode_by_id, name="redirect_qrcode_by_id"),
+    path("calender/", views.calender_overview, name="calender_overview"),
 ]
