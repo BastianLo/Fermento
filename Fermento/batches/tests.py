c3ef69f888980676c807422dff41e956332590a5
BastianLo <bastian.lobe@t-online.de>
2023-04-16 17:11:11 +0200

    Fix tests

--- Fermento/batches/tests.py
+++ Fermento/batches/tests.py
@@ -2,12 +2,14 @@ from django.test import TestCase
 
 
 from .models import QrCode, Batch
+from recipe_manager.models import recipe
 from django.contrib.auth.models import User
 
 class QrCodeTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser1', password='12345')
-        self.batch1 = Batch.objects.create(name="batch1", owner=self.user)
+        self.recipe1 = recipe.objects.create(name="kombucha", owner=self.user)
+        self.batch1 = Batch.objects.create(name="batch1", owner=self.user,related_recipe=self.recipe1)
         self.qr1 = QrCode.objects.create(name="qrcode1", owner=self.user, batch=self.batch1)
         self.qr2 = QrCode.objects.create(name="qrcode2", owner=self.user)
 
