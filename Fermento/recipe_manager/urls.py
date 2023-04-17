9b674d64ac619e9849f8bba61378e1240bb1dcfd
BastianLo <bastian.lobe@t-online.de>
2023-04-18 00:42:41 +0200

    Adjust url name

--- Fermento/recipe_manager/urls.py
+++ Fermento/recipe_manager/urls.py
@@ -8,7 +8,7 @@ handler400, handler403, handler404, handler500
 
 urlpatterns = [
     path("", RedirectView.as_view(url='recipe')),
-    path("recipe/", views.index, name="index"),
+    path("recipe/", views.index, name="recipeindex"),
     path("recipe/<int:recipe_id>/", views.recipe_by_id, name="recipe_by_id"),
     path("recipe/<int:recipe_id>/delete", views.delete_recipe_by_id, name="delete_recipe_by_id"),
     path("recipe/<int:recipe_id>/edit", views.edit_recipe_by_id, name="edit_recipe_by_id"),
--- Fermento/settings_manager/urls.py
+++ Fermento/settings_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("notification/", views.notifcation, name="index"),
-    path("notification/save", views.notifcation_save, name="index"),
+    path("notification/", views.notifcation, name="notifcation"),
+    path("notification/save", views.notifcation_save, name="notifcation_save"),
 ]
