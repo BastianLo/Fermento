f0ba4433feb28bccbf7dd28a85b9b841a3c84a01
BastianLo <bastian.lobe@t-online.de>
2023-04-19 17:53:21 +0200

    Task completion using js instead of form

---
+++ Fermento/static/custom/js/batches/complete_execution.js
@@ -0,0 +1,16 @@
+
+$(function () {
+    $(":button").click(function () {   
+        let container = $(this).parent()
+        $.ajax({
+            url: "/batches/executions/complete/" + $(this).attr('btnvalue') + "/",
+            method: "POST",
+            data: {
+                "csrfmiddlewaretoken":csrftoken
+            },
+            success: function (data) {
+                container.remove();
+            }
+        });
+    });
+});
