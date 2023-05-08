062040905193f3f3d2ed9baffb3c25bcc0e82dc3
BastianLo <bastian.lobe@t-online.de>
2023-05-08 16:58:48 +0200

    Language switcher remember language

--- client/src/components/common/languageSwitcher.vue
+++ client/src/components/common/languageSwitcher.vue
@@ -4,6 +4,7 @@
     v-model="locale"
     :options="localeOptions"
     :label="$t('language')"
+    @update:model-value="changeLanguage"
     dense
     borderless
     emit-value
@@ -16,13 +17,23 @@
 
 <script>
 import { useI18n } from 'vue-i18n';
+import { Quasar, useQuasar } from 'quasar';
+
+Quasar.lang.getLocale(); // returns a string
 
 export default {
   setup() {
     const { locale } = useI18n({ useScope: 'global' });
+    const $q = useQuasar();
+    //locale.value = $q.lang.getLocale();
+    locale.value = localStorage.getItem('selectedLanguage') || locale.value;
 
+    const changeLanguage = () => {
+      localStorage.setItem('selectedLanguage', locale.value);
+    };
     return {
       locale,
+      changeLanguage,
       localeOptions: [
         { value: 'en-US', label: 'English' },
         { value: 'de', label: 'Deutsch' },
